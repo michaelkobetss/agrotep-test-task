0b1bbb652dce92cf5408f1f9f59ebba9e95f57e7
michaelkobetss <michael.kobetss@gmail.com>
2023-12-17 23:01:45 +0200

    backup 5

--- api версія/front-end/index.js
+++ api версія/front-end/index.js
@@ -24,53 +24,47 @@ function switchTheme() {
 
 // Fetch data and fill the table
 async function fetchDataAndFillTable() {
-    try {
-      const response = await fetch("http://127.0.0.1:3000/api/data");
-      const data = await response.json();
-      const table = document.getElementById("data-table");
-  
-      // Create table head and body
-      createTableHead(table, data[0]);
-      createTableBody(table, data);
-  
-      // Call editTable again to update the table for editing
-    //   editTable();
-    } catch (error) {
-      console.error("Error:", error);
-    }
+  try {
+    const response = await fetch("http://127.0.0.1:3000/api/data");
+    const data = await response.json();
+    const table = document.getElementById("data-table");
+
+    // Create table head and body
+    createTableHead(table, data[0]);
+    createTableBody(table, data);
+  } catch (error) {
+    console.error("Error:", error);
   }
-  
-  
-  function createTableHead(table, dataItem) {
-    // Clear existing table head
-    table.tHead?.remove();
-  
-    // Create new table head
-    const thead = table.createTHead();
-    const headerRow = thead.insertRow();
-    Object.keys(dataItem).forEach((key) => {
-      const th = document.createElement("th");
-      th.textContent = key;
-      headerRow.appendChild(th);
+}
+
+function createTableHead(table, dataItem) {
+  // Clear existing table head
+  table.tHead?.remove();
+
+  // Create new table head
+  const thead = table.createTHead();
+  const headerRow = thead.insertRow();
+  Object.keys(dataItem).forEach((key) => {
+    const th = document.createElement("th");
+    th.textContent = key;
+    headerRow.appendChild(th);
+  });
+}
+
+function createTableBody(table, data) {
+  // Clear existing table body
+  table.tBodies[0]?.remove();
+
+  // Create new table body
+  const tbody = table.createTBody();
+  data.forEach((item) => {
+    const row = tbody.insertRow();
+    Object.values(item).forEach((value) => {
+      const cell = row.insertCell();
+      cell.textContent = value;
     });
-  }
-  
-  function createTableBody(table, data) {
-    // Clear existing table body
-    table.tBodies[0]?.remove();
-  
-    // Create new table body
-    const tbody = table.createTBody();
-    data.forEach((item) => {
-      const row = tbody.insertRow();
-      Object.values(item).forEach((value) => {
-        const cell = row.insertCell();
-        cell.textContent = value;
-      });
-    });
-  }
-  
-  
+  });
+}
 
 // Add a new row to the table
 async function addRow() {
@@ -126,8 +120,7 @@ function editTable() {
     const cell = headerRow.insertCell(i);
     const button = document.createElement("button");
     button.classList.add("btn");
-    button.innerHTML =
-      '<i class="fas fa-trash"></i><i class="fas fa-left-right"></i>';
+    button.innerHTML = '<i class="fas fa-trash"></i>';
     button.onclick = function () {
       deleteColumn(i);
     };
@@ -176,45 +169,42 @@ function finishEditing() {
   finishEditingButton.style.display = "none";
 
   // Get the headers (third row of the table)
-  const headers = Array.from(table.rows[1].cells).map((cell) => {
-    return cell.innerText; // Get the innerText instead of input value
-  });
+  const headers = Array.from(table.rows[1].cells)
+    .slice(1) // Skip the first index
+    .map((cell) => {
+      const input = cell.querySelector("input");
+      return input ? input.value : "";
+    });
 
-  // Iterate over all rows in the table, starting from the fourth row
+  console.log(headers);
   const data = Array.from(table.rows)
-    .slice(1) // Skip the first three rows
+    .slice(2) // Skip the first row
     .map((row) => {
       const rowData = {};
-      // Iterate over all cells in the row, skipping the first cell
-      console.log(row.cells)
-      Array.from(row.cells)
-        .slice(1)
-        .forEach((cell, index) => {
-          const input = cell.querySelector("input");
-          const tHeader = cell.querySelector("th")
-          // Check if the input is not null before accessing its value
-
+      // Iterate over all cells in the row
+      Array.from(row.cells).forEach((cell, index) => {
+        const input = cell.querySelector("input");
+        // Check if the input is not null before accessing its value
+        if (input) {
           // Set the property of the rowData object based on the header
-          if (input) {
-            rowData[index] = input.value;
-          }
-        });
+          rowData[headers[index - 1]] = input.value;
+        }
+      });
       return rowData;
     });
 
-    console.log(data)
+  console.log(data);
   // Send table data to server
-//   fetch("http://127.0.0.1:3000/api/update", {
-//     method: "PUT",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(data),
-//   }).then(location.reload());
+    fetch("http://127.0.0.1:3000/api/update", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then(location.reload());
 }
 
 async function deleteRow(rowIndex) {
-
   try {
     const response = await fetch(
       `http://127.0.0.1:3000/api/deleteRow/${rowIndex}`,
